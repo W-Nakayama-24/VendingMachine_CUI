49c5f0f1b27f1203d03b62c3b5aa9555372c0abd
Wataru.Nakayama <Wataru.Nakayama@jp.ricoh.com>
2019-11-06 15:18:32 +0900

    ADD class
    UserInterfaceクラス、自動販売機クラス、上限チェッカークラスを実装

--- Source/VendingMachine/src/pkg/Main.java
+++ Source/VendingMachine/src/pkg/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 
+        UserInterface ui = new UserInterface();
+        boolean systemContinueFlag = true;
+        while (systemContinueFlag == true) {
+            ui.display();
+            ui.callFunction();
+        }
+
     }
 }
